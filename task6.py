d5922b98f34248675c2eb208dcd4aff140fe38ce
EvgeniaLunina <100754299+EvgeniaLunina@users.noreply.github.com>
2022-10-16 00:49:11 +0300

    Лабораторная работа 3

---
+++ task6.py
@@ -0,0 +1,10 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+n = 10
+# TODO Оформить решение
+max_number = max(list_numbers)
+for i in range(n):
+    if list_numbers[i] == max_number:
+        index_max = i
+        break
+list_numbers[-1], list_numbers[index_max] = list_numbers[index_max], list_numbers[-1]
+print(list_numbers)
